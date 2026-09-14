d27fe716e35f9df1414b773c9d2eeadbdc3d122c
Caique Costa <ccostafrias@gmail.com>
2026-09-14 00:53:02 -0300

    feat(UI): sets a brightness shader for inUse elements

--- modules/constructors/uiscenes.lua
+++ modules/constructors/uiscenes.lua
@@ -214,8 +214,21 @@ function newEquipmentScene(player)
 	local lifebar = UILifeBarElem.new("equip player lifebar", vec(canvasCenter.x, canvasCenter.y - 170), size(74, 14), calcFunc, offset, 3)
 	equipScene:addElement(lifebar, ELEM_LAYER_1, vec(1, 5))
 
-	function equipScene:newEquipmentElement(equipment, pos)
-		local element = UIImageElem.new("equip " .. equipment.name, pos, size(96, 96))
+	-- cria os UIElements de fato (armas e artefatos são interagíveis)
+	function equipScene:newEquipmentElement(equipmentType, equipment, pos)
+		local element
+		if equipmentType == BLESSING then
+			element = UIImageElem.new("equip " .. equipment.name, pos, size(96, 96))
+		else
+			element = UIButtonElem.new("equip " .. equipment.name, pos, size(96, 96), nil, function()
+				if equipmentType == WEAPON then
+					player:equipWeapon(equipment.name)
+				elseif equipmentType == ARTIFACT then
+					player:equipArtifact(equipment.name)
+				end
+				equipScene:setEquipmentInUse(equipmentType, equipment)
+			end)
+		end
 		element.equipment = equipment
 
 		-- TODO: botar animação certinha
@@ -223,6 +236,28 @@ function newEquipmentScene(player)
 		return element
 	end
 
+	-- coloca em uso o elemento equipado e retira todos os outros
+	function equipScene:setEquipmentInUse(equipmentType, equippedEquipment)
+		local equipmentList = equipmentType == WEAPON and player.weapons or player.artifacts
+
+		for idx, equipment in ipairs(equipmentList) do
+			local _, layerPos = self:getEquipmentSlotPosition(equipmentType, idx)
+			local element = self.layers[ELEM_LAYER_2][layerPos.y] and self.layers[ELEM_LAYER_2][layerPos.y][layerPos.x]
+			local slot = self.layers[ELEM_LAYER_1][layerPos.y] and self.layers[ELEM_LAYER_1][layerPos.y][layerPos.x]
+
+			-- equipa o elemento em si
+			if element then
+				element:setInUse(equipment == equippedEquipment)
+			end
+
+			-- equipa o slot
+			if slot then
+				slot:setInUse(equipment == equippedEquipment)
+			end
+		end
+	end
+
+	-- sincroniza os equipamentos do player com o que é visto na UI
 	function equipScene:syncEquipment(equipmentType, equipmentList)
 		-- varre os equipamentos atuais e vê se já foram criados na UI
 		for idx, equipment in ipairs(equipmentList) do
@@ -233,7 +268,7 @@ function newEquipmentScene(player)
 				if current then
 					self:removeElement(ELEM_LAYER_2, layerPos)
 				end
-				self:addElement(self:newEquipmentElement(equipment, pos), ELEM_LAYER_2, layerPos)
+				self:addElement(self:newEquipmentElement(equipmentType, equipment, pos), ELEM_LAYER_2, layerPos)
 			end
 		end
 
@@ -257,6 +292,8 @@ function newEquipmentScene(player)
 		self:syncEquipment(WEAPON, self.player.weapons)
 		self:syncEquipment(ARTIFACT, self.player.artifacts)
 		self:syncEquipment(BLESSING, self.player.blessingManager.equipped)
+		self:setEquipmentInUse(WEAPON, self.player.weapon)
+		self:setEquipmentInUse(ARTIFACT, self.player.artifact)
 	end
 
 	return equipScene
